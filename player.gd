4788d77da9b3b948d01a90aeeb4847247b92b76f
Quentin Jungo <quentin.jungo@gmail.com>
2022-11-06 14:29:06 +0100

    new

--- player.gd
+++ player.gd
@@ -22,20 +22,20 @@ func _ready():
 	_animated_sprite.stop()
 
 func get_input():
-    velocity = Vector2()
-    if Input.is_action_pressed("ui_right"):
-        velocity.x += 1
-    if Input.is_action_pressed("ui_left"):
-        velocity.x -= 1
-    if Input.is_action_pressed("ui_down"):
-        velocity.y += 1
-    if Input.is_action_pressed("ui_up"):
-        velocity.y -= 1
-    velocity = velocity.normalized() * speed
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
-    get_input()
-    velocity = move_and_slide(velocity)
+	get_input()
+	velocity = move_and_slide(velocity)
 
 func animation():
 	if Input.is_action_pressed("ui_up") && Input.is_action_pressed("ui_right"):
